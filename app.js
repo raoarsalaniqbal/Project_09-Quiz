ecd65783e96569c8f62b3ad806e7e86f4f81107f
raoarsalaniqbal <142326719+raoarsalaniqbal@users.noreply.github.com>
2023-11-05 14:41:53 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+#!usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+let a = await inquirer.prompt({
+    type: "input",
+    name: "fname",
+    message: "What is your name",
+});
+console.log(chalk.blue(a.fname));
+let b = await inquirer.prompt({
+    type: "input",
+    name: "fname",
+    message: "Where Are You From?",
+});
+console.log(chalk.blue(b.fname));
+let c = await inquirer.prompt({
+    type: "input",
+    name: "fname",
+    message: "What is Your Education?",
+});
+console.log(chalk.blue(c.fname));
